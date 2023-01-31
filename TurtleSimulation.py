cec3a60eafcf860e9f6bd5139a18cfc5e0cb88f6
Cody Grogan <A02313514@usu.edu>
2023-01-31 14:48:54 -0700

    Made code more modular. Seperated AStarPlanner and Simulation

---
+++ TurtleSimulation.py
@@ -0,0 +1,65 @@
+import numpy as np
+import torch as T
+
+from Agent import Agent
+from Simulation import Simulation
+
+def main():
+    print(__file__ + " start!!")
+    grid_size = 2  # [m]
+    robot_radius = 1.0  # [m]
+    obstacle_count = 5
+
+    sim = Simulation(robot_radius, grid_size, obstacle_count)
+
+    agent = Agent(
+        alpha=0.000025,
+        beta=0.00025,
+        input_dims=[12],
+        tau=0.001,
+        batch_size=64,
+        fc1_dims=300,
+        fc2_dims=200,
+        fc3_dims=100,
+        n_actions=2,
+        action_range=1,
+        run_name="2"
+    )
+    agent.load_models()
+    print("Using GPU: {}".format(T.cuda.is_available()))
+
+    score_history = []
+    for i in range(1000):
+        done = False
+        score = 0
+        obs = sim.reset()
+        while not done:
+            act = agent.choose_action(obs)
+            new_state, reward, done = sim.step(act)
+            #agent.remember(obs, act, reward, new_state, int(done))
+            #agent.learn()
+            score += reward
+            obs = new_state
+            if score < -8000:
+                break
+
+        score_history.append(score)
+        print(
+            "episode",
+            i,
+            "score %.2f" % score,
+            "100 game average %.2f" % np.mean(score_history[-100:]),
+        )
+
+        # if i % 25 == 0:
+        #     agent.save_models()
+        #     sim.plot_res(score_history, "Learning Rate Graphs", 2000, int(sys.argv[1]))
+        if i % 1 == 0:
+            sim.showPath()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
--- simulation.py
+++ simulation.py
@@ -1,266 +1,27 @@
-import math
-import os.path
-import sys
-
-import numpy
-import numpy as np
 import random
-from IPython.display import clear_output
-import torch.cuda
-import torch as T
+import numpy as np
+import math
 import matplotlib.pyplot as plt
 
-from Agent import Agent
-show_animation = True
-
-
-class AStarPlanner:
-
-    def __init__(self, ox, oy, resolution, rr):
-        """
-        Initialize grid map for a star planning
-
-        ox: x position list of Obstacles [m]
-        oy: y position list of Obstacles [m]
-        resolution: grid resolution [m]
-        rr: robot radius[m]
-        """
-
-        self.resolution = resolution
-        self.rr = rr
-        self.min_x, self.min_y = 0, 0
-        self.max_x, self.max_y = 0, 0
-        self.obstacle_map = None
-        self.x_width, self.y_width = 0, 0
-        self.motion = self.get_motion_model()
-        self.calc_obstacle_map(ox, oy)
-
-    class Node:
-        def __init__(self, x, y, cost, parent_index):
-            self.x = x  # index of grid
-            self.y = y  # index of grid
-            self.cost = cost
-            self.parent_index = parent_index
-
-        def __str__(self):
-            return str(self.x) + "," + str(self.y) + "," + str(
-                self.cost) + "," + str(self.parent_index)
-
-    def planning(self, sx, sy, gx, gy):
-        """
-        A star path search
-
-        input:
-            s_x: start x position [m]
-            s_y: start y position [m]
-            gx: goal x position [m]
-            gy: goal y position [m]
-
-        output:
-            rx: x position list of the final path
-            ry: y position list of the final path
-        """
-
-        start_node = self.Node(self.calc_xy_index(sx, self.min_x),
-                               self.calc_xy_index(sy, self.min_y), 0.0, -1)
-        goal_node = self.Node(self.calc_xy_index(gx, self.min_x),
-                              self.calc_xy_index(gy, self.min_y), 0.0, -1)
-
-        open_set, closed_set = dict(), dict()
-        open_set[self.calc_grid_index(start_node)] = start_node
-
-        while 1:
-            if len(open_set) == 0:
-                print("Open set is empty..")
-                break
-
-            c_id = min(
-                open_set,
-                key=lambda o: open_set[o].cost + self.calc_heuristic(goal_node,
-                                                                     open_set[
-                                                                         o]))
-            current = open_set[c_id]
-
-            # show graph
-            # if show_animation:  # pragma: no cover
-            #     plt.plot(self.calc_grid_position(current.x, self.min_x),
-            #              self.calc_grid_position(current.y, self.min_y), "xc")
-            #     # for stopping simulation with the esc key.
-            #     plt.gcf().canvas.mpl_connect('key_release_event',
-            #                                  lambda event: [exit(
-            #                                      0) if event.key == 'escape' else None])
-            #     if len(closed_set.keys()) % 10 == 0:
-            #         plt.pause(0.001)
-
-            if current.x == goal_node.x and current.y == goal_node.y:
-                goal_node.parent_index = current.parent_index
-                goal_node.cost = current.cost
-                break
-
-            # Remove the item from the open set
-            del open_set[c_id]
-
-            # Add it to the closed set
-            closed_set[c_id] = current
-
-            # expand_grid search grid based on motion model
-            for i, _ in enumerate(self.motion):
-                node = self.Node(current.x + self.motion[i][0],
-                                 current.y + self.motion[i][1],
-                                 current.cost + self.motion[i][2], c_id)
-                n_id = self.calc_grid_index(node)
-
-                # If the node is not safe, do nothing
-                if not self.verify_node(node):
-                    continue
-
-                if n_id in closed_set:
-                    continue
-
-                if n_id not in open_set:
-                    open_set[n_id] = node  # discovered a new node
-                else:
-                    if open_set[n_id].cost > node.cost:
-                        # This path is the best until now. record it
-                        open_set[n_id] = node
-
-        rx, ry = self.calc_final_path(goal_node, closed_set)
-
-        return rx, ry 
-
-    def calc_final_path(self, goal_node, closed_set):
-        # generate final course
-        rx, ry = [self.calc_grid_position(goal_node.x, self.min_x)], [
-            self.calc_grid_position(goal_node.y, self.min_y)]
-        parent_index = goal_node.parent_index
-        while parent_index != -1:
-            n = closed_set[parent_index]
-            rx.append(self.calc_grid_position(n.x, self.min_x))
-            ry.append(self.calc_grid_position(n.y, self.min_y))
-            parent_index = n.parent_index
-
-        return rx, ry
-
-    @staticmethod
-    def calc_heuristic(n1, n2):
-        w = 1.0  # weight of heuristic
-        d = w * math.hypot(n1.x - n2.x, n1.y - n2.y)
-        return d
-
-    def calc_grid_position(self, index, min_position):
-        """
-        calc grid position
-
-        :param index:
-        :param min_position:
-        :return:
-        """
-        pos = index * self.resolution + min_position
-        return pos
-
-    def calc_xy_index(self, position, min_pos):
-        return round((position - min_pos) / self.resolution)
-
-    def calc_grid_index(self, node):
-        return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
-
-    def verify_node(self, node):
-        px = self.calc_grid_position(node.x, self.min_x)
-        py = self.calc_grid_position(node.y, self.min_y)
-
-        if px < self.min_x:
-            return False
-        elif py < self.min_y:
-            return False
-        elif px >= self.max_x:
-            return False
-        elif py >= self.max_y:
-            return False
-
-        # collision check
-        if self.obstacle_map[node.x][node.y]:
-            return False
-
-        return True
-
-    def calc_obstacle_map(self, ox, oy):
-
-        self.min_x = round(min(ox))
-        self.min_y = round(min(oy))
-        self.max_x = round(max(ox))
-        self.max_y = round(max(oy))
-        # print("min_x:", self.min_x)
-        # print("min_y:", self.min_y)
-        # print("max_x:", self.max_x)
-        # print("max_y:", self.max_y)
-
-        self.x_width = round((self.max_x - self.min_x) / self.resolution)
-        self.y_width = round((self.max_y - self.min_y) / self.resolution)
-        # print("x_width:", self.x_width)
-        # print("y_width:", self.y_width)
-
-        # obstacle map generation
-        self.obstacle_map = [[False for _ in range(self.y_width)]
-                             for _ in range(self.x_width)]
-        for ix in range(self.x_width):
-            x = self.calc_grid_position(ix, self.min_x)
-            for iy in range(self.y_width):
-                y = self.calc_grid_position(iy, self.min_y)
-                for iox, ioy in zip(ox, oy):
-                    d = math.hypot(iox - x, ioy - y)
-                    if d <= self.rr:
-                        self.obstacle_map[ix][iy] = True
-                        break
-
-    @staticmethod
-    def get_motion_model():
-        # dx, dy, cost
-        motion = [[1, 0, 1],
-                  [0, 1, 1],
-                  [-1, 0, 1],
-                  [0, -1, 1],
-                  [-1, -1, math.sqrt(2)],
-                  [-1, 1, math.sqrt(2)],
-                  [1, -1, math.sqrt(2)],
-                  [1, 1, math.sqrt(2)]]
-
-        return motion
-
-def normalDistribution(minValue, maxValue):
-    mid = minValue + (maxValue - minValue) / 2
-    std_dev = (maxValue - mid) / 2
-
-    return round(np.random.normal(loc=mid, scale=std_dev))
+from AStarPlanner import AStarPlanner
 
 class Simulation:
-    def __init__(self, robot_radius, grid_size, obstacle_count, sx, sy, gx, gy):
+    def __init__(self, robot_radius, grid_size, obstacle_count):
         self.robot_radius = robot_radius
         self.grid_size = grid_size
         self.obstacle_count = obstacle_count
-        self.gx = gx
-        self.gy = gy
-        self.sx = sx
-        self.sy = sy
-        self.pnt2 = 1
-        self.theta = [math.pi / 2]
         self.dist_tolerance = 3*robot_radius
-        self.pnt = 0
         self.steps = 0
         self.shapes = ['box']
         self.pathDistance = 50
         self.cone_angle = 15
-        self.getMap()
-        self.tx = [sx]
-        self.ty = [sy]
-        self.ix = [sx]
-        self.iy = [sy]
-        self.goal_magnitude = None
+        self.reset()
         angle = np.linspace( 0 , 2 * np.pi , 150 )
         radius = self.robot_radius - 0.5
-        xpoints = sx + radius * np.cos( angle )
-        ypoints = sy + radius * np.sin( angle )
+        xpoints = self.sx + radius * np.cos( angle )
+        ypoints = self.sy + radius * np.sin( angle )
         self.robot, = plt.plot(xpoints, ypoints, marker = ".", color = 'r')
-        self.robotAngle, = plt.plot(sx + radius * np.cos( self.theta[-1] ), sy + radius * np.sin( self.theta[-1]  ), marker = ".", color = 'b')
+        self.robotAngle, = plt.plot(self.sx + radius * np.cos( self.theta[-1] ), self.sy + radius * np.sin( self.theta[-1]  ), marker = ".", color = 'b')
 
     def __motion(
         self, v_left: float, v_right: float, x: float, y: float, theta: float, dt: float
@@ -545,12 +306,12 @@ class Simulation:
         gx, gy = self.getGoal()
 
         if self.goal_magnitude != None:
-            goal = 125 * (self.goal_magnitude - numpy.sqrt(gx*gx + gy * gy))
+            goal = 125 * (self.goal_magnitude - np.sqrt(gx*gx + gy * gy))
         else:
             goal = 0
-        self.goal_magnitude = numpy.sqrt(gx * gx + gy * gy)
+        self.goal_magnitude = np.sqrt(gx * gx + gy * gy)
 
-        path_distance = numpy.sqrt(x*x + y*y)
+        path_distance = np.sqrt(x*x + y*y)
         off_path = 2 * (self.robot_radius - path_distance)
         off_path_sparse = 0
         if path_distance > 7 * self.robot_radius:
@@ -635,6 +396,7 @@ class Simulation:
         except KeyboardInterrupt:
             print("Visualization exited")
             return
+        plt.close()
 
 
     def distance(self, x1, y1, x2, y2):
@@ -663,7 +425,8 @@ class Simulation:
         obs_1, obs_2, obs_3, obs_4 = self.getObstacle()
         return [path_x, path_y, g_x/goal_distance, g_y/goal_distance, obs_1, obs_2, obs_3, obs_4, self.getTheta(), self.ix[-1], self.iy[-1], self.theta[-1]]
 
-    def plot_res(self, values, title='', goal=1000, run_number=0):
+    @staticmethod
+    def plot_res(values, title='', goal=1000, run_number=0):
         ''' Plot the reward curve and histogram of results over time.'''
         # Update the window after each episode
         clear_output(wait=True)
@@ -698,74 +461,3 @@ class Simulation:
             os.path.join(dir_name, fig_name)
         )
         plt.close(f)
-
-
-
-
-def main():
-    print(__file__ + " start!!")
-            # start and goal position
-    sx = random.randint(3, 67)  # [m]
-    sy = random.randint(3, 67)  # [m]
-    gx = random.randint(3, 67)  # [m]
-    gy = random.randint(3, 67)  # [m]
-    grid_size = 2  # [m]
-    robot_radius = 1.0  # [m]
-    obstacle_count = 5
-
-    sim = Simulation(robot_radius, grid_size, obstacle_count, sx, sy, gx, gy)
-
-    agent = Agent(
-        alpha=0.000025,
-        beta=0.00025,
-        input_dims=[12],
-        tau=0.001,
-        batch_size=64,
-        fc1_dims=300,
-        fc2_dims=200,
-        fc3_dims=100,
-        n_actions=2,
-        action_range=1,
-        run_name=sys.argv[1]
-    )
-    agent.load_models()
-    print(T.cuda.is_available())
-    print(torch.cuda.get_device_name(0))
-    np.random.seed(0)
-
-    score_history = []
-    for i in range(1000):
-        done = False
-        score = 0
-        obs = sim.reset()
-        while not done:
-            act = agent.choose_action(obs)
-            new_state, reward, done = sim.step(act)
-            #agent.remember(obs, act, reward, new_state, int(done))
-            #agent.learn()
-            score += reward
-            obs = new_state
-            if score < -8000:
-                break
-
-        score_history.append(score)
-        print(
-            "episode",
-            i,
-            "score %.2f" % score,
-            "100 game average %.2f" % np.mean(score_history[-100:]),
-        )
-
-        # if i % 25 == 0:
-        #     agent.save_models()
-        #     sim.plot_res(score_history, "Learning Rate Graphs", 2000, int(sys.argv[1]))
-        if i % 1 == 0:
-            sim.showPath()
-            plt.close()
-
-
-
-
-
-if __name__ == '__main__':
-    main()
